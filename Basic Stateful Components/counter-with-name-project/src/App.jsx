9a586ac3fdbcccfdce4712ed3b0a9a0a2dec8766
David Phillipus <david.n.phillipus@gmail.com>
2026-09-07 08:51:12 +0200

    Add Basic Stateful Components\counter-with-name-project\src\App.jsx

---
+++ Basic Stateful Components/counter-with-name-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { Component } from "./Component";
+
+function App() {
+  return (
+    <>
+      <Component />
+    </>
+  );
+}
+
+export default App;
