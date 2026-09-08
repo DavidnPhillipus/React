638671191612fc2c669ca9c02ba346e85088657c
David Phillipus <david.n.phillipus@gmail.com>
2026-09-09 01:38:31 +0200

    Add Components Lifecycles\use-effect-exercises

---
+++ Components Lifecycles/use-effect-exercises/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import React from "react";
+import Child from "./Child";
+
+export default function App() {
+  const [show, setShow] = useState(true);
+
+  const childComponent = show ? <Child /> : null;
+
+  return (
+    <div>
+      <button onClick={() => setShow((currentShow) => !currentShow)}>
+        Show/Hide
+      </button>
+      {childComponent}
+    </div>
+  );
+}
