11693f05e5f558389e6b05fd73cfc584aa472f5c
David Phillipus <david.n.phillipus@gmail.com>
2026-09-20 13:20:00 +0200

    Add What Is React example

---
+++ WhatIsReact.jsx
@@ -0,0 +1,18 @@
+/**
+ * React is a JavaScript library for building user interfaces. It allows developers
+ * to create reusable UI components and manage the state and lifecycle of these components
+ * efficiently. React uses a declarative approach, making it easier to reason about the UI
+ * and its state. It also employs a virtual DOM to optimize rendering performance.
+ *
+ * This component/function is part of a React application and is designed to handle
+ * specific functionality or render a portion of the UI.
+ */
+
+
+/**
+ * While JavaScript is inherently an imperative programming language, React introduces
+ * a declarative paradigm for building user interfaces. In imperative programming, developers
+ * write code that explicitly describes the steps to achieve a desired outcome. React, on the
+ * other hand, allows developers to describe what the UI should look like, and it takes care
+ * of updating the DOM to match that description.
+ */
